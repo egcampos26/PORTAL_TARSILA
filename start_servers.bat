04da1cbf7c1b578556914c6be6517c2873c2b800
Eduardo Campos <egcampos@gmail.com>
2026-02-02 23:47:00 -0300

    Update start_servers.bat to force ports 3001 and 3002

--- start_servers.bat
+++ start_servers.bat
@@ -1,10 +1,10 @@
-
 @echo off
-echo Starting Carometro Alunos...
-start "Carometro Alunos" /d "..\..\Carometro dos Alunos" npm run dev
+echo Starting Carometro Alunos (Port 3001)...
+start "Carometro Alunos" /d "..\..\Carometro dos Alunos" npm run dev -- --port 3001 --strictPort
 
-echo Starting Carometro Funcionarios...
-start "Carometro Funcionarios" /d "..\..\Crometro dos Funcionarios\CAROMETRO_FUNCIONARIOS" npm run dev
+echo Starting Carometro Funcionarios (Port 3002)...
+start "Carometro Funcionarios" /d "..\..\Crometro dos Funcionarios\CAROMETRO_FUNCIONARIOS" npm run dev -- --port 3002 --strictPort
 
 echo All services started!
+echo Please check the newly opened terminal windows for any errors.
 pause
